3c3d3f392c9ad72243172bde77aa53c173b1cff4
echewisi <josephechewisi@gmail.com>
2023-09-18 23:15:20 +0100

    minor changes

--- base/models.py
+++ base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+class Profile(User):
+    accesstoken= models.CharField(null= True, blank= True)
+    refreshtoken= models.CharField(null=True, blank= True)
 # Create your models here.
