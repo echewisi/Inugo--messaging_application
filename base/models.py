de4046fb7c377c15b7d9cc367b4a25ae4e01daa6
echewisi <josephechewisi@gmail.com>
2023-09-19 22:19:33 +0100

    small changes

--- base/models.py
+++ base/models.py
@@ -4,4 +4,14 @@ from django.contrib.auth.models import User
 class Profile(User):
     accesstoken= models.CharField(null= True, blank= True)
     refreshtoken= models.CharField(null=True, blank= True)
+
+
+
+class Message(models.Model):
+    content = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    sent_at = models.DateTimeField(auto_now_add=True)
+    read_at = models.DateTimeField(null=True, blank=True)
+
 # Create your models here.
--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+
+class MessageApiView(APIView):
+    
+    def post(self, request):
+
 # Create your views here.
